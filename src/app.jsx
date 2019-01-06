0273bd989ce926c8f313b65c284d4bae91fb37a9
Michael Meow <dearmichaelmeow@gmail.com>
2019-01-06 11:20:18 -0800

    add initial app with redux store

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (<div>
+      <h2>Let's make an electron app with react and redux-saga</h2>
+    </div>);
+  }
+}
